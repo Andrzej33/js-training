da9cb99bcabd1311e72c8ad36239d861c7593f0d
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-01-01 17:59:11 +0200

    2 lesson

--- module-1/index.js
+++ module-1/index.js
@@ -1,2 +1,14 @@
-console.log("lesson lack");
 
+
+// function includes(array, value) {
+ 
+// for (let element of array) {
+//   if (element === value) {
+//     return true;
+//   }
+//   return false;
+// }
+ 
+// }
+console.log(disafhgv)
+console.log(hello);
